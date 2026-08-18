6f11be96bfb4ced5135bb05f3181028870a4e591
Svelge <ground.control.p@gmail.com>
2026-08-18 20:57:40 +0300

    IlCarro (WebDriverWait)

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,6 +8,8 @@ class LoginPage:
     PASSWORD_INPUT = (By.CSS_SELECTOR,"[type='password']")
     LOGIN_BTN = (By.CSS_SELECTOR,"[type='submit']")
     SIGN_OUT_BTN = (By.XPATH,"//button[text()='Log out']")
+    CONFIRMATION_TEXT = (By.CSS_SELECTOR,"h3")
+    OK_BTN = (By.XPATH,"//*[text()='OK']")
 
     def __init__(self,driver):
         self.driver = driver
@@ -29,3 +31,22 @@ class LoginPage:
 
     def submit_login(self):
         self.driver.find_element(*self.LOGIN_BTN).click()
+
+    def login(self,email,password):
+        self.fill_email(email)
+        self.fill_password(password)
+        self.submit_login()
+
+    def login_success_text(self):
+        # return self.driver.find_element(*self.CONFIRMATION_TEXT).text
+        element = WebDriverWait(self.driver,timeout=5).until(
+            EC.visibility_of_element_located(self.CONFIRMATION_TEXT))
+        return element.text
+
+    def close_window(self):
+        self.driver.find_element(*self.OK_BTN).click()
+
+    def is_logged(self):
+        wait = WebDriverWait(self.driver, 5)
+        sign_out_btn_element = wait.until(EC.visibility_of_element_located(self.SIGN_OUT_BTN))
+        return True
--- tests/test_login.py
+++ tests/test_login.py
@@ -10,3 +10,6 @@ def test_login_success(driver):
     login_page.fill_email(VALID_EMAIL)
     login_page.fill_password(VALID_PASSWORD)
     login_page.submit_login()
+    assert login_page.login_success_text() == "You are logged in success"
+    login_page.close_window()
+    assert login_page.is_logged() is True
